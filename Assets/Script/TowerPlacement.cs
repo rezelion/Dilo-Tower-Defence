ee23642f02ff72ea66ce1651320f7f4572baec29
rezelion <ahmadspade2014@gmail.com>
2021-09-19 20:59:28 +0700

    Initial commit

---
+++ Assets/Script/TowerPlacement.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerPlacement : MonoBehaviour
+{
+    private Tower _placedTower;
+
+    // Fungsi yang terpanggil sekali ketika ada object Rigidbody yang menyentuh area collider
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (_placedTower != null && _placedTower.IsPlaced && !_placedTower.gameObject.activeSelf)
+        {
+            _placedTower = null;
+        }
+
+        if (_placedTower == null)
+        {
+            Tower tower = collision.GetComponent<Tower>();
+            if (tower != null)
+            {
+                tower.SetPlacePosition(transform.position);
+                _placedTower = tower;
+            }
+        }
+    }
+    // Kebalikan dari OnTriggerEnter2D, fungsi ini terpanggil sekali ketika object tersebut meninggalkan area collider
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (_placedTower == null) return;
+
+        if (!_placedTower.IsPlaced)
+        {
+            _placedTower.SetPlacePosition(null);
+            _placedTower = null;
+        }
+
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
